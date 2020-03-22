dc389e865305087270d1a812524fd6ed37fe74b2
Themis <themiscadiz@gmail.com>
2020-03-21 22:33:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,11 +35,6 @@ app.use(express.static( path.resolve(__dirname, 'views') ));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({extended:true})); // to support URL-encoded bodies
 
-
-// app.get("/", (request, response) => {
-//     response.sendFile("index.html");
-// });
-
 //send static HTML file, 
 app.get('/', function(req, res) {
     res.sendFile(indexLocation)
@@ -49,20 +44,6 @@ let cat = 0;
 let dog = 0;
 let bird = 0;
 let fish = 0;
-
-
-// GET - /api get everything
-// app.get("/api", (request, response) => {    
-//     // db references our nedb instance
-//     // we use "find" and an empty search {} to give us back all the data in the db
-//     db.find({}, function (err, docs) {
-//         if(err){
-//             return err;
-//         } 
-//         // like before we send the json response
-//         response.json(docs);
-//     });
-// });
 
 
 //*********** test MongoDB
@@ -77,32 +58,6 @@ app.get("/api/", async (req, res) => {
     }
   });
 
-// app.get("/api/v1/todos", async (req, res) => {
-//     try{
-//       res.json({})
-//     } catch(error){
-//       console.error(error);
-//       res.json(error);
-//     }
-//   });
-
-
-// ****************
-//To add a new element to db
-// app.post("/api", (request, response) => {
-//     // our unix timestamp
-//     const unixTimeCreated = new Date().getTime();
-//     // add our unix time as a "created" property and add it to our request.body
-//     const newData = Object.assign({"created": unixTimeCreated}, request.body)
-
-//     // add in our data object to our database using .insert()
-//     db.insert(newData, (err, docs) =>{
-//         if(err){
-//             return err;
-//         }
-//         response.json(docs);
-//     });
-// })
 
 // To ADD an item to the database
 // POST: "api/v1/todos"
@@ -113,11 +68,6 @@ app.post("/api/", async (req, res) => {
         votes: req.body.votes
       }
 
-        // // our unix timestamp
-        // const unixTimeCreated = new Date().getTime();
-        // // add our unix time as a "created" property and add it to our request.body
-        // const newData = Object.assign({"created": unixTimeCreated}, req.body)
-
         const data = await todos.create(newData);
         res.json(data);
     } catch(error){
@@ -125,96 +75,6 @@ app.post("/api/", async (req, res) => {
       res.json(error);
     }
   });
-
-
-
-// // PUT - /api
-// app.put("/api/:id", (request, response)=> {
-
-//     // we get the id of the item we want from request.params.id ==> this matches the :id of the URL parameter
-//     const selectedItemId = request.params.id;
-
-//     const updatedDataProperties = request.body
-    
-
-//     //If the selected id is a CAT, then update in the database
-//     if(selectedItemId == "tP9eCA3nl6VeJBu9"){
-
-
-//         cat=request.body.votes;
-
-//         // Set an existing field's value
-//         db.update({ _id: selectedItemId  }, { $set: updatedDataProperties }, (err, numReplaced) => {
-//             if(err){
-//                 response.status(404).send("not working");
-//             }
-//         });
-//         response.json(
-//         {message: cat}
-//         )
-
-//     }    
-//     //If the selected id is a DOG, then update in the database
-//     else if(selectedItemId == "b4mkpyQp3c2zhcXd"){
-
-//         // console.log(selectedItemId);
-//         // console.log("it's a dog");
-
-//         dog=request.body.votes;
-
-//         // Set an existing field's value
-//         db.update({ _id: selectedItemId  }, { $set: updatedDataProperties }, (err, numReplaced) => {
-//             if(err){
-//                 response.status(404).send("not working");
-//             }
-           
-//         });
-//         response.json(
-//         {message: dog}
-//         )
-//     }
-
-// //If the selected id is a BIRD, then update in the database
-// else if(selectedItemId == "aWq4oGUP71nivULK"){
-
-//     // console.log(selectedItemId);
-
-//     bird=request.body.votes;
-
-//     // Set an existing field's value
-//     db.update({ _id: selectedItemId  }, { $set: updatedDataProperties }, (err, numReplaced) => {
-//         if(err){
-//             response.status(404).send("not working");
-//         }
-       
-//     });
-//     response.json(
-//     {message: bird}
-//     )
-// }
-
-
-// //If the selected id is a FISH, then update in the database
-// else if(selectedItemId == "PMgfiYc84P0FZwDb"){
-
-//     // console.log(selectedItemId);
-
-//     fish=request.body.votes;
-
-//     // Set an existing field's value
-//     db.update({ _id: selectedItemId  }, { $set: updatedDataProperties }, (err, numReplaced) => {
-//         if(err){
-//             response.status(404).send("not working");
-//         }
-       
-//     });
-//     response.json(
-//     {message: fish}
-//     )
-// }
-
-// }
-// );
 
 // To UPDATE votes
 // PUT: "api/v1/todos:id"
@@ -224,8 +84,6 @@ app.put("/api/:id", async (req, res) => {
 
     const updatedData = req.body;
         
-    //If the selected id is a CAT, then update in the database
-    // if(selectedItemId == "5e7698eff41339db0c5e0f14"){
         
         try{
            
@@ -237,27 +95,6 @@ app.put("/api/:id", async (req, res) => {
             console.error(error);
             res.json(error);
         }
-    // }  
-    // //If the selected id is a DOG, then update in the database
-    // else if(selectedItemId == "5e769cb990b622dbb789ae4e"){
-        
-    //     try{
-           
-    //         const data = await todos.findOneAndUpdate({_id: selectedItemId}, updatedData, {new:true});
-    //         res.json(
-    //             {message: data.votes}
-    //         )
-    //     } catch(error){
-    //         console.error(error);
-    //         res.json(error);
-    //     }
-    // }                
-
-
-
-
-
-    
   });
 
 
